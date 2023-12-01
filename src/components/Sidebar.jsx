c8dd167df610bf217b11b60bbf53a6ece80d3b3c
unknown <fghalandarii@gmail.com>
2023-12-02 01:37:15 +0330

    Initial components

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,28 @@
+const Sidebar = () => {
+  return (
+    <div className="flex ">
+      <div>
+        <span>GraphHub</span>
+      </div>
+      <div>
+        <u>
+          <li>
+            <span>Dashboard</span>
+          </li>
+          <li>
+            <span>Dashboard</span>
+          </li>
+          <li>
+            <span>Dashboard</span>
+          </li>
+          <li>
+            <span>Dashboard</span>
+          </li>
+        </u>
+      </div>
+      <div>color options</div>
+    </div>
+  );
+};
+
+export default Sidebar;

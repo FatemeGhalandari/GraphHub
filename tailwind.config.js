435607bcd691667b8d60e0d898bf8f1a528ed6af
unknown <fghalandarii@gmail.com>
2023-12-07 00:44:31 +0330

    Add color configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,11 +8,16 @@ module.exports = {
       colors: {
         primary: "#000000",
         secondary: "#ffffff",
-        dimWhite: "rgba(255, 255, 255, 0.7)",
-        dimBlue: "rgba(9, 151, 124, 0.1)",
+        textColor: "#555",
+        borderColor: "rgb(230,227,227)",
+        darkGreen: "#166534",
+        green: "#16a34a",
+        lightGreen: "#ecfccb",
+        dimGray: "#e5e5e5",
       },
       fontFamily: {
         poppins: ["Poppins", "sans-serif"],
+        Nunito: ["Nunito", "sans-serif"],
       },
     },
     screens: {

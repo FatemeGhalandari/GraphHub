44391090713147437bdf5a15a145925b17b64b4c
unknown <fghalandarii@gmail.com>
2023-12-07 00:42:48 +0330

    Define all the styles

---
+++ src/components/styles.js
@@ -0,0 +1,11 @@
+const styles = {
+  logo: "text-[20px] text-darkGreen font-bold",
+  paragraph: "text-[13px] font-bold text-green mt-[15px] mb-[5px]",
+  li: "flex items-center p-[5px] cursor-pointer hover:bg-lightGreen",
+  icon: "text-[18px] text-darkGreen mr-2",
+  span: "text-[14px] text-textColor font-normal",
+  colorOptions:
+    "h-[20px] w-[20px] border-[1px] cursor-pointer border-darkGreen border-solid rounded-md m-[5px]",
+  borderBottom: " border-solid border-b-[0.5px] border-b-borderColor",
+};
+export default styles;

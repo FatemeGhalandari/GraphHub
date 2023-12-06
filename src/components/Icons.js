680f010d2e1381ee41063109d3706110320770a2
unknown <fghalandarii@gmail.com>
2023-12-07 00:42:21 +0330

    Import and export  all icons

---
+++ src/components/Icons.js
@@ -0,0 +1,38 @@
+import DashboardIcon from "@mui/icons-material/Dashboard";
+import PersonIcon from "@mui/icons-material/Person";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import SettingsIcon from "@mui/icons-material/Settings";
+import LogoutIcon from "@mui/icons-material/Logout";
+import StackedLineChartIcon from "@mui/icons-material/StackedLineChart";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import HealthAndSafetyIcon from "@mui/icons-material/HealthAndSafety";
+import LocalShippingIcon from "@mui/icons-material/LocalShipping";
+import InventoryIcon from "@mui/icons-material/Inventory";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import PsychologyIcon from "@mui/icons-material/Psychology";
+import SearchIcon from "@mui/icons-material/Search";
+import LanguageIcon from "@mui/icons-material/Language";
+import DarkModeIcon from "@mui/icons-material/DarkMode";
+import FullscreenIcon from "@mui/icons-material/Fullscreen";
+import ChatIcon from "@mui/icons-material/Chat";
+import FormatListBulletedIcon from "@mui/icons-material/FormatListBulleted";
+export {
+  DashboardIcon,
+  PersonIcon,
+  AccountCircleIcon,
+  SettingsIcon,
+  LogoutIcon,
+  StackedLineChartIcon,
+  NotificationsIcon,
+  HealthAndSafetyIcon,
+  LocalShippingIcon,
+  InventoryIcon,
+  ShoppingCartIcon,
+  PsychologyIcon,
+  SearchIcon,
+  LanguageIcon,
+  DarkModeIcon,
+  FullscreenIcon,
+  ChatIcon,
+  FormatListBulletedIcon,
+};

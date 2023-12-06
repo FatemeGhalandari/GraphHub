1760f434662ce84c74e640c448b0e11e50645e40
unknown <fghalandarii@gmail.com>
2023-12-07 00:45:03 +0330

    Add Navbar componetn

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,14 @@
+import Navbar from "../components/Navbar";
 import Sidebar from "../components/Sidebar";
 
 const Home = () => {
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-row">
       <Sidebar />
-      <div className="bg-green-300">container</div>
+      <div className="flex-[6]">
+        <Navbar />
+        container
+      </div>
     </div>
   );
 };

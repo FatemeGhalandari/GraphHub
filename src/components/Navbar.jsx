4cf678aa712f1a4d0b77e74a2111b7609ce408e9
unknown <fghalandarii@gmail.com>
2023-12-07 00:46:06 +0330

    Add styles and components

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,51 @@
+import {
+  SearchIcon,
+  LanguageIcon,
+  DarkModeIcon,
+  FullscreenIcon,
+  NotificationsIcon,
+  ChatIcon,
+  FormatListBulletedIcon,
+} from "../components/Icons";
+import styles from "./styles";
 const Navbar = () => {
   return (
-    <div>
-      
+    <div
+      className={`${styles.borderBottom} h-[50px] flex flex-row items-center text-[14px] text-textColor`}
+    >
+      <div className="flex flex-row items-center px-[20px] justify-between w-[100%]">
+        <div className="flex flex-row items-center">
+          <input
+            type="text"
+            placeholder="Search..."
+            className="border-none outline-none bg-transparent text-[13px]"
+          />
+          <SearchIcon />
+        </div>
+        <div className="flex flex-row ">
+          <div className="flex flex-row">
+            <LanguageIcon />
+            <p>English</p>
+          </div>
+          <div>
+            <DarkModeIcon />
+          </div>
+          <div>
+            <FullscreenIcon />
+          </div>
+          <div>
+            <NotificationsIcon />
+          </div>
+          <div>
+            <ChatIcon />
+          </div>
+          <div>
+            <FormatListBulletedIcon />
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;

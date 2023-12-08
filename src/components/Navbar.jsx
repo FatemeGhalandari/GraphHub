9c3480e10ead6a371213b168cff2ffe7d7f168c6
unknown <fghalandarii@gmail.com>
2023-12-09 01:25:58 +0330

    Fix search and nav icons

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,39 +8,52 @@ import {
   FormatListBulletedIcon,
 } from "../components/Icons";
 import styles from "./styles";
+import avatar from "../assets/avatar.svg";
+
 const Navbar = () => {
   return (
     <div
       className={`${styles.borderBottom} h-[50px] flex flex-row items-center text-[14px] text-textColor`}
     >
       <div className="flex flex-row items-center px-[20px] justify-between w-[100%]">
-        <div className="flex flex-row items-center">
+        <div
+          className={`${styles.fullBorder} flex flex-row items-center p-[3px] `}
+        >
           <input
             type="text"
             placeholder="Search..."
             className="border-none outline-none bg-transparent text-[13px]"
           />
-          <SearchIcon />
+          <SearchIcon className={`${styles.navIcons}`} />
         </div>
         <div className="flex flex-row ">
-          <div className="flex flex-row">
-            <LanguageIcon />
-            <p>English</p>
+          <div className={`${styles.navItem}`}>
+            <LanguageIcon className={`${styles.navIcons}`} />
+            <p className="ml-1">English</p>
           </div>
-          <div>
-            <DarkModeIcon />
+          <div className={`${styles.navItem}`}>
+            <DarkModeIcon className={`${styles.navIcons}`} />
           </div>
-          <div>
-            <FullscreenIcon />
+          <div className={`${styles.navItem}`}>
+            <FullscreenIcon className={`${styles.navIcons}`} />
           </div>
-          <div>
-            <NotificationsIcon />
+          <div className={`${styles.navItem} relative`}>
+            <NotificationsIcon className={`${styles.navIcons}`} />
+            <div className={`${styles.notif}`}>2</div>
           </div>
-          <div>
-            <ChatIcon />
+          <div className={`${styles.navItem} relative`}>
+            <ChatIcon className={`${styles.navIcons}`} />{" "}
+            <div className={`${styles.notif}`}>1</div>
           </div>
-          <div>
-            <FormatListBulletedIcon />
+          <div className={`${styles.navItem}`}>
+            <FormatListBulletedIcon className={`${styles.navIcons}`} />
+          </div>
+          <div className={`${styles.navItem}`}>
+            <img
+              src={avatar}
+              alt="avatar"
+              className={`${styles.navIcons} w-[30px] h-[30px] rounded-[50%]`}
+            />
           </div>
         </div>
       </div>

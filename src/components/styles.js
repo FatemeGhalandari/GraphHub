f8c80bcbe3cf0b45829605fa65214d6e625a3849
unknown <fghalandarii@gmail.com>
2023-12-09 01:26:28 +0330

    Add nav icons and nav items stles

--- src/components/styles.js
+++ src/components/styles.js
@@ -7,5 +7,10 @@ const styles = {
   colorOptions:
     "h-[20px] w-[20px] border-[1px] cursor-pointer border-darkGreen border-solid rounded-md m-[5px]",
   borderBottom: " border-solid border-b-[0.5px] border-b-borderColor",
+  fullBorder: "border-solid border-[0.5px] border-borderColor",
+  navIcons: "text-[18px] text-textColor cursor-pointer",
+  navItem: "flex items-center mr-[20px]",
+  notif:
+    "bg-[#dc2626] rounded-[50%] text-[10px] text-white w-[15px] h-[15px] flex items-center justify-center top-[0] right-[0] absolute",
 };
 export default styles;

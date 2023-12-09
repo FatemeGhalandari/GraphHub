b2043f3a6c00396ebf983a2e3e297414b8ed6861
unknown <fghalandarii@gmail.com>
2023-12-09 18:01:58 +0330

    Update to edit colors

--- src/components/styles.js
+++ src/components/styles.js
@@ -3,13 +3,13 @@ const styles = {
   paragraph: "text-[13px] font-bold text-green mt-[15px] mb-[5px]",
   li: "flex items-center p-[5px] cursor-pointer hover:bg-lightGreen",
   icon: "text-[18px] text-darkGreen mr-2",
-  span: "text-[14px] text-textColor font-normal",
+  span: "text-[14px] text-navItemColor font-normal",
   colorOptions:
     "h-[20px] w-[20px] border-[1px] cursor-pointer border-darkGreen border-solid rounded-md m-[5px]",
   borderBottom: " border-solid border-b-[0.5px] border-b-borderColor",
   fullBorder: "border-solid border-[0.5px] border-borderColor",
-  navIcons: "text-[18px] text-textColor cursor-pointer",
-  navItem: "flex items-center mr-[20px]",
+  navIcons: "text-[18px] cursor-pointer",
+  navItem: "flex items-center mr-[20px] text-navItemColor",
   notif:
     "bg-[#dc2626] rounded-[50%] text-[10px] text-white w-[15px] h-[15px] flex items-center justify-center top-[0] right-[0] absolute",
 };

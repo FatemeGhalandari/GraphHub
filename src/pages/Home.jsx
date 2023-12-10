86968a7860aeb2cbfe238470217bccbe5d698166
unknown <fghalandarii@gmail.com>
2023-12-10 21:46:45 +0330

    Update to add table component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import Chart from "../components/Chart";
 import Featured from "../components/Featured";
 import Navbar from "../components/Navbar";
 import Sidebar from "../components/Sidebar";
+import Table from "../components/Table";
 import Widget from "../components/Widget";
 import styles from "../components/styles";
 
@@ -17,9 +18,15 @@ const Home = () => {
           <Widget type="earnings" />
           <Widget type="balance" />
         </div>
-        <div className={`${styles.parent} px-5 py-2`}>
+        <div className={`${styles.parent}`}>
           <Featured />
           <Chart />
+        </div>
+        <div className={`${styles.boxShadow} px-5 py-5 mx-5 my-1 mb-4`}>
+          <p className="text-textColor text-[18px] font-semibold mb-2 ">
+            Latest Transactions
+          </p>
+          <Table />
         </div>
       </div>
     </div>

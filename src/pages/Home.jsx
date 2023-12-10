179da3d7c33875f8a3e60c81b6a6d7726ebb5ed6
unknown <fghalandarii@gmail.com>
2023-12-10 16:42:41 +0330

    Add Featured and chart components in home container

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,9 @@
+import Chart from "../components/Chart";
+import Featured from "../components/Featured";
 import Navbar from "../components/Navbar";
 import Sidebar from "../components/Sidebar";
 import Widget from "../components/Widget";
+import styles from "../components/styles";
 
 const Home = () => {
   return (
@@ -8,11 +11,15 @@ const Home = () => {
       <Sidebar />
       <div className="flex-[6]">
         <Navbar />
-        <div className="flex p-5">
+        <div className={styles.parent}>
           <Widget type="users" />
           <Widget type="orders" />
           <Widget type="earnings" />
           <Widget type="balance" />
+        </div>
+        <div className={`${styles.parent} px-5 py-2`}>
+          <Featured />
+          <Chart />
         </div>
       </div>
     </div>

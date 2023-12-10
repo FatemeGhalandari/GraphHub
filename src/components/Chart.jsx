de766c8f1c9a4a11e27bdaa19f7ee6899a787ec9
unknown <fghalandarii@gmail.com>
2023-12-10 16:44:11 +0330

    Chart component in home container

---
+++ src/components/Chart.jsx
@@ -0,0 +1,13 @@
+import styles from "./styles";
+
+const Chart = () => {
+  return (
+    <div
+      className={`${styles.boxShadow} flex flex-[2]`}
+    >
+      chart
+    </div>
+  );
+};
+
+export default Chart;

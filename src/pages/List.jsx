14f1f7077f68ab64fa26dc30506fb835ab0bbab8
unknown <fghalandarii@gmail.com>
2023-12-12 20:23:38 +0330

    List of users and products page

--- src/pages/List.jsx
+++ src/pages/List.jsx
@@ -1,5 +1,16 @@
+import Datatable from "../components/Datatable";
+import Navbar from "../components/Navbar";
+import Sidebar from "../components/Sidebar";
 const Home = () => {
-  return <div>list</div>;
+  return (
+    <div className="flex flex-row w-[100%]">
+      <Sidebar />
+      <div className="flex-[6]">
+        <Navbar />
+        <Datatable />
+      </div>
+    </div>
+  );
 };
 
 export default Home;

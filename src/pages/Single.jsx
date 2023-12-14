37055afc27c61421192f43e47006b69d852bef1f
unknown <fghalandarii@gmail.com>
2023-12-14 23:38:48 +0330

    Single page component

--- src/pages/Single.jsx
+++ src/pages/Single.jsx
@@ -1,10 +1,28 @@
-
+import Navbar from "../components/Navbar";
+import Sidebar from "../components/Sidebar";
+import styles from "../components/styles";
 const Single = () => {
   return (
-    <div>
-      
+    <div className="flex flex-row w-[100%]">
+      <Sidebar />
+      <div className="flex-[6]">
+        <Navbar />
+        <div className={`${styles.boxShadow} ${styles.parent}`}>
+          <div className="flex flex-[1] flex-col gap-5">
+            <h2 className="text-[15px] text-textColor">information</h2>
+            <img src="" alt="" />
+            <h1>Jane Doe</h1>
+            <p>Email: janedoe@gmail.com</p>
+            <p>Phone: +1 234 567 890</p>
+            <p>Address: </p>
+            <p>Country: USA</p>
+          </div>
+          <div className={`flex flex-[2] ${styles.parent}`}>right</div>
+        </div>
+        <div className="flex p-5">bottom</div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Single
+export default Single;

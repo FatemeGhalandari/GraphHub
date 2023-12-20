29f6904412af7b76ee052facf4276aa0e87e55f5
unknown <fghalandarii@gmail.com>
2023-12-20 23:20:00 +0330

    Update to add darkmode

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -60,37 +60,45 @@ const Tables = () => {
     },
   ];
   return (
-    <TableContainer component={Paper}>
+    <TableContainer component={Paper} className="dark:bg-[#222] ">
       <Table sx={{ minWidth: 650 }} aria-label="simple table">
         <TableHead>
           <TableRow>
-            <TableCell>Tracking ID</TableCell>
-            <TableCell>Product</TableCell>
-            <TableCell>Customer</TableCell>
-            <TableCell>Date</TableCell>
-            <TableCell>Amount</TableCell>
-            <TableCell>Payment Method</TableCell>
-            <TableCell>Status</TableCell>
+            <TableCell className=" dark:text-textColor ">Tracking ID</TableCell>
+            <TableCell className=" dark:text-textColor">Customer</TableCell>
+            <TableCell className=" dark:text-textColor">Date</TableCell>
+            <TableCell className=" dark:text-textColor">Product</TableCell>
+            <TableCell className=" dark:text-textColor">Amount</TableCell>
+            <TableCell className=" dark:text-textColor">
+              Payment Method
+            </TableCell>
+            <TableCell className=" dark:text-textColor">Status</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
           {rows.map((row) => (
             <TableRow key={row.id}>
-              <TableCell>{row.id}</TableCell>
-              <TableCell>
+              <TableCell className=" dark:text-textColor">{row.id}</TableCell>
+              <TableCell className=" dark:text-textColor">
                 <div className="flex flex-row items-center">
                   <img
                     src={row.img}
                     alt="row"
-                    className=" h-[32px] w-[32px] rounded-[50%] object-cover mr-2"
+                    className=" h-[32px] w-[32px] rounded-[50%] object-cover mr-2 "
                   />
                   {row.product}
                 </div>
               </TableCell>
-              <TableCell>{row.customer}</TableCell>
-              <TableCell>{row.date}</TableCell>
-              <TableCell>{row.amount}</TableCell>
-              <TableCell>{row.method}</TableCell>
+              <TableCell className=" dark:text-textColor">
+                {row.customer}
+              </TableCell>
+              <TableCell className=" dark:text-textColor">{row.date}</TableCell>
+              <TableCell className=" dark:text-textColor">
+                {row.amount}
+              </TableCell>
+              <TableCell className=" dark:text-textColor">
+                {row.method}
+              </TableCell>
               <TableCell>
                 <span
                   className={`p-2 rounded-md ${

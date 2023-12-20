cbbb607f4312f154334cb74f1f2b9fb926e2d0b4
unknown <fghalandarii@gmail.com>
2023-12-20 23:19:44 +0330

    Update to add darkmode

--- src/components/styles.js
+++ src/components/styles.js
@@ -3,17 +3,19 @@ const styles = {
   paragraph: "text-[13px] font-bold text-green mt-[15px] mb-[5px]",
   li: "flex items-center p-[5px] cursor-pointer hover:bg-lightGreen",
   icon: "text-[18px] text-darkGreen mr-2",
-  span: "text-[14px] text-navItemColor font-normal",
+  span: "text-[14px] text-navItemColor font-normal dark:text-textColor",
   colorOptions:
     "h-[20px] w-[20px] border-[1px] cursor-pointer border-darkGreen border-solid rounded-md m-[5px]",
-  borderBottom: " border-solid border-b-[0.5px] border-b-borderColor",
-  fullBorder: "border-solid border-[0.5px] border-borderColor",
-  navIcons: "text-[18px] cursor-pointer",
+  borderBottom:
+    " border-solid border-b-[0.5px] border-b-borderColor dark:border-b-navItemColor",
+  fullBorder:
+    "border-solid border-[0.5px] border-borderColor dark:border-navItemColor",
+  navIcons: "text-[18px] cursor-pointer dark:text-textColor",
   navItem: "flex items-center mr-[20px] text-navItemColor",
   notif:
     "bg-[#dc2626] rounded-[50%] text-[10px] text-white w-[15px] h-[15px] flex items-center justify-center top-[0] right-[0] absolute",
   parent: "flex p-5 gap-5",
-  boxShadow: "shadow-md shadow-slate-200 ",
+  boxShadow: "shadow-md shadow-slate-200 dark:shadow-navItemColor",
   userInfo: "mb-4 text-[14px] flex flex-row gap-2",
   userInfoKey: "font-bold text-navItemColor",
   userInfoValue: "font-light",

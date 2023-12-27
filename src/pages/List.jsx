69b3e51fafa4bc9111458ca333544ed628c8cb98
unknown <fghalandarii@gmail.com>
2023-12-27 20:06:48 +0330

    Update to add dark mode

--- src/pages/List.jsx
+++ src/pages/List.jsx
@@ -3,7 +3,7 @@ import Navbar from "../components/Navbar";
 import Sidebar from "../components/Sidebar";
 const Home = () => {
   return (
-    <div className="flex flex-row w-[100%]">
+    <div className="flex flex-row w-[100%] dark:bg-[#222] dark:text-textColor">
       <Sidebar />
       <div className="flex-[6]">
         <Navbar />

b4a938aae53db1772edf45673a0df896ba0dd608
unknown <fghalandarii@gmail.com>
2023-12-29 02:09:16 +0330

    Update to import api key from .env

--- src/firebase.js
+++ src/firebase.js
@@ -1,11 +1,9 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_KEY,
+  apiKey: import.meta.env.VITE_API_KEY,
   authDomain: "graphhub-ad5d8.firebaseapp.com",
   projectId: "graphhub-ad5d8",
   storageBucket: "graphhub-ad5d8.appspot.com",
@@ -14,4 +12,6 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+const firebase = initializeApp(firebaseConfig);
+export default firebase;
+export const auth = getAuth();

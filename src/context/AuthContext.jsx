68febe7154f11d8008901affe9962f7759a672ef
unknown <fghalandarii@gmail.com>
2023-12-29 17:48:56 +0330

    Add authentication context and add user to local storage

---
+++ src/context/AuthContext.jsx
@@ -0,0 +1,29 @@
+import { createContext, useEffect, useReducer } from "react";
+import AuthReducer from "./AuthReducer";
+import PropTypes from "prop-types";
+
+// initial state
+const INITIAL_STATE = {
+  currentUser: JSON.parse(localStorage.getItem("user")) || null,
+};
+
+// create context
+const AuthContext = createContext(INITIAL_STATE);
+
+// provider component
+const AuthProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(AuthReducer, INITIAL_STATE);
+  useEffect(() => {
+    localStorage.setItem("user", JSON.stringify(state.currentUser));
+  }, [state.currentUser]);
+
+  return (
+    <AuthContext.Provider value={{ currentUser: state.currentUser, dispatch }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+AuthProvider.propTypes = {
+  children: PropTypes.any.isRequired,
+};
+export { AuthContext, AuthProvider };

f851c8bf72e48a77488211049dace979f0e66f6b
unknown <fghalandarii@gmail.com>
2024-01-14 18:12:40 +0330

    Add collectionName and columns as props so functions work for all collections

--- src/pages/List.jsx
+++ src/pages/List.jsx
@@ -1,13 +1,19 @@
 import Datatable from "../components/Datatable";
 import Navbar from "../components/Navbar";
 import Sidebar from "../components/Sidebar";
+import { userColumns, productColumns } from "../components/datatablSource";
+import { useLocation } from "react-router-dom";
 const Home = () => {
+  let location = useLocation().pathname;
   return (
     <div className="flex flex-row w-[100%] dark:bg-[#222] dark:text-textColor">
       <Sidebar />
       <div className="flex-[6]">
         <Navbar />
-        <Datatable />
+        <Datatable
+          collectionName={location === "/products" ? "products" : "users"}
+          columns={location === "/products" ? productColumns : userColumns}
+        />
       </div>
     </div>
   );

3a0f0cdce2b480de12bf92d59f599b2fb716a27f
unknown <fghalandarii@gmail.com>
2024-01-14 18:00:52 +0330

    Add unsub function to show data from database

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -5,69 +5,38 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
+import { useEffect, useState } from "react";
+import { db } from "../firebase";
+import { collection, onSnapshot } from "firebase/firestore";
 
 const Tables = () => {
-  const rows = [
-    {
-      id: 174827,
-      product: "Acer Nitro 5",
-      img: "https://m.media-amazon.com/images/I/81bc8mA3nKL._AC_UY327_FMwebp_QL65_.jpg",
-      customer: "John Doe",
-      date: "2023-12-10",
-      amount: 50.0,
-      method: "Credit Card",
-      status: "Completed",
-    },
-    {
-      id: 643722,
-      product: "Playstation 5",
-      img: "https://m.media-amazon.com/images/I/31JaiPXYI8L._AC_UY327_FMwebp_QL65_.jpg",
-      customer: "Jane Smith",
-      date: "2023-12-11",
-      amount: 75.0,
-      method: "PayPal",
-      status: "Pending",
-    },
-    {
-      id: 234873,
-      product: "Redragon S101",
-      img: "https://m.media-amazon.com/images/I/71kr3WAj1FL._AC_UY327_FMwebp_QL65_.jpg",
-      customer: "Bob Johnson",
-      date: "2023-12-12",
-      amount: 30.0,
-      method: "Debit Card",
-      status: "Completed",
-    },
-    {
-      id: 460213,
-      product: "Razer Blade 15",
-      img: "https://m.media-amazon.com/images/I/71wF7YDIQkL._AC_UY327_FMwebp_QL65_.jpg",
-      customer: "Alice Williams",
-      date: "2023-12-13",
-      amount: 60.0,
-      method: "Bank Transfer",
-      status: "Completed",
-    },
-    {
-      id: 128328,
-      product: "ASUS ROG Strix",
-      img: "https://m.media-amazon.com/images/I/81hH5vK-MCL._AC_UY327_FMwebp_QL65_.jpg",
-      customer: "Charlie Brown",
-      date: "2023-12-14",
-      amount: 25.0,
-      method: "Cash",
-      status: "Pending",
-    },
-  ];
+  const [data, setData] = useState([]);
+  const unsubscribed = async () => {
+    await onSnapshot(collection(db, "orders"), (snapshot) => {
+      let tempData = [];
+      snapshot.docs.forEach(
+        (doc) => {
+          tempData.push({ id: doc.id, ...doc.data() });
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+      setData(tempData);
+    });
+  };
+  useEffect(() => {
+    unsubscribed();
+  }, []);
   return (
     <TableContainer component={Paper} className="dark:bg-[#222] ">
       <Table sx={{ minWidth: 650 }} aria-label="simple table">
         <TableHead>
           <TableRow>
-            <TableCell className=" dark:text-textColor ">Tracking ID</TableCell>
+            <TableCell className=" dark:text-textColor">Tracking ID</TableCell>
+            <TableCell className=" dark:text-textColor">Product</TableCell>
             <TableCell className=" dark:text-textColor">Customer</TableCell>
             <TableCell className=" dark:text-textColor">Date</TableCell>
-            <TableCell className=" dark:text-textColor">Product</TableCell>
             <TableCell className=" dark:text-textColor">Amount</TableCell>
             <TableCell className=" dark:text-textColor">
               Payment Method
@@ -76,38 +45,44 @@ const Tables = () => {
           </TableRow>
         </TableHead>
         <TableBody>
-          {rows.map((row) => (
-            <TableRow key={row.id}>
-              <TableCell className=" dark:text-textColor">{row.id}</TableCell>
+          {data.map((order) => (
+            <TableRow key={order.id}>
+              <TableCell className=" dark:text-textColor">{order.id}</TableCell>
               <TableCell className=" dark:text-textColor">
                 <div className="flex flex-row items-center">
                   <img
-                    src={row.img}
+                    src={order.img}
                     alt="row"
                     className=" h-[32px] w-[32px] rounded-[50%] object-cover mr-2 "
                   />
-                  {row.product}
+                  {order.product}
                 </div>
               </TableCell>
               <TableCell className=" dark:text-textColor">
-                {row.customer}
+                {order.customer}
               </TableCell>
-              <TableCell className=" dark:text-textColor">{row.date}</TableCell>
-              <TableCell className=" dark:text-textColor">
-                {row.amount}
+              <TableCell className="dark:text-textColor">
+                {order.date && order.date.seconds
+                  ? // Format the date here based on your requirements
+                    new Date(order.date.seconds * 1000).toLocaleDateString()
+                  : // Handle the case where date is not present or not in the expected format
+                    "N/A"}
               </TableCell>
               <TableCell className=" dark:text-textColor">
-                {row.method}
+                {order.amount}
+              </TableCell>
+              <TableCell className=" dark:text-textColor">
+                {order.method}
               </TableCell>
               <TableCell>
                 <span
                   className={`p-2 rounded-md ${
-                    row.status === "Completed"
+                    order.status === "Completed"
                       ? "bg-[#d1fae5] text-darkGreen"
                       : "bg-[#fef9c3] text-[#eab308]"
                   }`}
                 >
-                  {row.status}
+                  {order.status}
                 </span>
               </TableCell>
             </TableRow>
